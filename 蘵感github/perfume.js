222e0988ff4ec8b4bbf3f5e369c31e934472ca9b
machi <joanna881110@gmail.com>
2023-05-15 17:04:03 +0800

    0515

--- 蘵感github/perfume.js
+++ 蘵感github/perfume.js
@@ -1,1 +1,7 @@
 // <!-- FAQ -->
+// Toggle Collapse
+$('.faq li .question').click(function () {
+	$(this).find('.plus-minus-toggle').toggleClass('collapsed');
+	$(this).parent().toggleClass('active');
+  });
+  
