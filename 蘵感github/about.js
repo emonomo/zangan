eb995bb2d134e1b2e9146fba3e77d25b3dd70550
NANANA <joanna881110@gmail.com>
2023-05-19 06:48:16 +0800

    0518r

---
+++ 蘵感github/about.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+//     // 取得圖片的寬度
+//     var logoWidth = $('.marquee-logo').width();
+  
+//     // 設定容器的寬度為圖片寬度的兩倍，以填滿橫條
+//     $('.marquee-container').width(logoWidth * 5);
+  
+//     // 複製圖片元素，使圖片重複出現
+//     $('.marquee-logo').clone().appendTo('.marquee-container');
+//   });
+  
