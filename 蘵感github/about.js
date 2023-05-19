b31c4fe2c4a162961fd2fc2b665355f8434ffcf8
machi <joanna881110@gmail.com>
2023-05-19 17:03:40 +0800

    0519

--- 蘵感github/about.js
+++ 蘵感github/about.js
@@ -1,11 +1,7 @@
-// $(document).ready(function() {
-//     // 取得圖片的寬度
-//     var logoWidth = $('.marquee-logo').width();
-  
-//     // 設定容器的寬度為圖片寬度的兩倍，以填滿橫條
-//     $('.marquee-container').width(logoWidth * 5);
-  
-//     // 複製圖片元素，使圖片重複出現
-//     $('.marquee-logo').clone().appendTo('.marquee-container');
-//   });
+// <!-- FAQ -->
+// Toggle Collapse
+$('.faq li .question').click(function () {
+	$(this).find('.plus-minus-toggle').toggleClass('collapsed');
+	$(this).parent().toggleClass('active');
+  });
   
