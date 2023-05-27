dfc74aec7600a59905d88ba5ed816e0bb85e458d
NANANA <joanna881110@gmail.com>
2023-05-27 15:46:37 +0800

    0527暫時沒有hover

--- 蘵感github/about.js
+++ 蘵感github/about.js
@@ -1,6 +1,47 @@
+//滾動至指定位置啟動動畫
+/*$(window).scroll(function () {
+	var h = $(window).height() - 800;
+
+	if ($(this).scrollTop() > h) {
+		$('.card:nth-child(1)').css("animation-name", "card-group_1");
+		$('.card:nth-child(2)').css("animation-name", "card-group_2");
+		$('.card:nth-child(3)').css("animation-name", "card-group_3");
+		$('.card:nth-child(4)').css("animation-name", "card-group_4"); 
+		}
+});
+*/
+$(window).scroll(function () {
+	var h = $(window).height() - 800;
+  
+	if ($(this).scrollTop() > h) {
+	  $('.card:nth-child(1)').css("animation-name", "card-group_1").one("animationend", function() {
+		$(this).addClass("animation-finished");
+	  });
+	  $('.card:nth-child(2)').css("animation-name", "card-group_2").one("animationend", function() {
+		$(this).addClass("animation-finished");
+	  });
+	  $('.card:nth-child(3)').css("animation-name", "card-group_3").one("animationend", function() {
+		$(this).addClass("animation-finished");
+	  });
+	  $('.card:nth-child(4)').css("animation-name", "card-group_4").one("animationend", function() {
+		$(this).addClass("animation-finished");
+	  });
+	} else {
+	  $('.card').removeClass("animation-finished").css("animation-name", "");
+	}
+  });
+  
 
 
-
+//  圖片淡入動畫  <!-- jQuery Smoove -->
+$('.smoove').smoove({
+	offset:'40%',
+	// moveX: '200px',
+	// moveY: '200px'
+ });
+ $('.smoove_2').smoove({
+	offset:'45%',
+ });
 
 
 // <!-- FAQ -->
